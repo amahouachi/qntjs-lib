9d0e61702b683c858fdb54032cac7d39a83eab47
Ahmed Mahouachi <levieux@gmail.com>
2026-01-13 12:32:26 +0100

    chore: export TA option types from package root

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ export * as ta from './ta/index.js';
 export * as arr from './arr/arr.js';
 export * as stats from './stats/index.js';
 
+export type { KstOptions, UltoscOptions, IchimokuOptions } from './ta/index.js';
+
 
